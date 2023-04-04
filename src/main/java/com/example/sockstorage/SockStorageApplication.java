9697c615969a9e210cd7e1855c186641bb39f14f
EvgenyFisenkoJava <evgeny.f.aircelle@gmail.com>
2023-04-04 13:36:52 +0200

    Initial commit

---
+++ src/main/java/com/example/sockstorage/SockStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.sockstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SockStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sockstorage/SockStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sockstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
